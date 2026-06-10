8ca78dc8b982ed3bedb69216904babcee6a20bea
Saurabh Kumar <saurabhshbm11@gmail.com>
2026-06-10 19:24:51 +0530

    Fixed CustomerSuccessPage explicit wait

--- BankingAutomationFramework/src/main/java/pages/guru99/CustomerSuccessPage.java
+++ BankingAutomationFramework/src/main/java/pages/guru99/CustomerSuccessPage.java
@@ -4,7 +4,11 @@ import base.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import utilities.TestContext;
-
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 public class CustomerSuccessPage extends BasePage {
 
     public CustomerSuccessPage(WebDriver driver) {
@@ -13,11 +17,20 @@ public class CustomerSuccessPage extends BasePage {
 
     public String getCustomerId() {
 
+        WebDriverWait wait =
+                new WebDriverWait(
+                        driver,
+                        Duration.ofSeconds(20));
+
+        WebElement customerElement =
+                wait.until(
+                        ExpectedConditions
+                                .visibilityOfElementLocated(
+                                        By.xpath(
+                                                "//td[text()='Customer ID']/following-sibling::td")));
+
         String customerId =
-                driver.findElement(
-                        By.xpath(
-                                "//td[text()='Customer ID']/following-sibling::td"))
-                        .getText();
+                customerElement.getText();
 
         TestContext.setCustomerId(customerId);
 
